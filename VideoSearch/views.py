259ccfa18b772307ce423fce403d987ec3595f83
deorhan <deorhan@edu.aau.at>
2025-06-20 14:44:05 +0200

    fixed performance issues

--- VideoSearch/views.py
+++ VideoSearch/views.py
@@ -5,13 +5,24 @@ from .models import Clip
 def home_view(request):
     query = request.GET.get("q")
     clips = []
+    video_ids = []
+    
     if query:
         clips = Clip.objects.filter(video__file_path__icontains=query)
+        seen_urls = []
+        
+        for clip in clips:
+            if clip.video.media_url not in seen_urls:
+                seen_urls.append(clip.video.media_url)
+        video_ids = seen_urls
+
     context = {
         "query": query,
         "clips": clips,
-        "start_time": 0
+        "start_time": 0,
+        "video_ids": video_ids
     }
+
     return render(request, "home.html", context)
 
 def detailed_view(request):
