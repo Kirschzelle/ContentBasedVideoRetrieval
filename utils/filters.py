1fb68cce7221712795201bd3ff4c832a27a9df5f
deorhan <deorhan@edu.aau.at>
2025-06-26 17:15:42 +0200

    drag and drop url update

--- utils/filters.py
+++ utils/filters.py
@@ -1,8 +1,8 @@
 from VideoSearch.utils.embeddings import calculate_combined_distance as filter_embedding_dist
 from VideoSearch.utils.color_features import compute_distance as filter_colors_dist
 
-def filter_embedding(canidate_features, filter_features):
-    return filter_embedding_dist(canidate_features, filter_features)
+def filter_embedding(candidate_features, filter_features):
+    return filter_embedding_dist(candidate_features, filter_features)
 
-def filter_colors(canidate_features, filter_features):
-    return filter_colors_dist(canidate_features, filter_features)
+def filter_colors(candidate_features, filter_features):
+    return filter_colors_dist(candidate_features, filter_features)
