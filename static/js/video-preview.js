77aed3e3c62b53d3249a33b81510030775c54271
Kirschzelle <nikolassc@edu.aau.at>
2025-09-04 11:40:40 +0200

    video looping early and keyfram offset wrong fixed

--- static/js/video-preview.js
+++ static/js/video-preview.js
@@ -1,31 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let isDragging = false;
 
     const container = document.querySelector(".preview-container");
     const video = document.querySelector("video");
-
-    const startFrame = parseInt(container.dataset.clipStartFrame);
-    const endFrame = parseInt(container.dataset.clipEndFrame);
     const fps = parseFloat(container.dataset.fps);
-    const startSeconds = startFrame / fps;
-    const endSeconds = endFrame / fps;
     const keyframeStartFrame = parseFloat(container.dataset.startFrame);
-    const keyframeStartTime = (startFrame + keyframeStartFrame) / fps;
+    const clipStartFrame = parseFloat(container.dataset.clipStartFrame);
+    const keyframeStartTime = (clipStartFrame + keyframeStartFrame) / fps;
 
-    video.addEventListener("loadedmetadata", () => {
+    video.addEventListener("loadeddata", () => {
         video.volume = 0.08;
         video.currentTime = keyframeStartTime;
-    });
-
-    video.addEventListener("timeupdate", () => {
-        if (!isDragging) {
-            if (video.currentTime >= endSeconds) {
-                video.pause();
-                video.currentTime = startSeconds;
-                setTimeout(() => {
-                    video.play().catch(() => { });
-                }, 100);
-            }
-        }
     });
 });
